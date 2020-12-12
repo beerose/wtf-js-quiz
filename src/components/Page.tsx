e38d09cafc16bc9e30316b901f6d828357dce401
Aleksandra Sikora <aleksandra@hasura.io>
2020-12-12 22:22:02 +0100

    update favicon

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -45,10 +45,7 @@ export function Page({ children }: PageProps) {
           href="https://fonts.googleapis.com/css2?family=Oswald&family=Andika+New+Basic:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⁉️</text></svg>"
-        />
+        <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
       </Helmet>
       <Global
         styles={{
