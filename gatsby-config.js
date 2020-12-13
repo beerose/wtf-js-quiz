2082f129ca60c8ae81dd6a07a2cf3f4cb53122ef
Aleksandra Sikora <aleksandra@hasura.io>
2020-12-13 10:51:10 +0100

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,11 @@
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-149605117-2',
+      },
+    },
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-typescript',
     {
